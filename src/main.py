22faac0839ee603acdecb23769cba91cc30361f9
jbarneyy <js_barnett@outlook.com>
2025-08-02 13:25:13 -0700

    Added call_function() to main.py to call types.FunctionCall functions from gemini

--- src/main.py
+++ src/main.py
@@ -22,9 +22,9 @@ from poi import POI
 
 def main():
 
-    iron_sword = Item("Iron Sword", ItemType.WEAPON, 5, "A simple iron sword.")
-    fish = Item("Small Fish", ItemType.MISC, 0, "A small blue fish.")
-    mead = Item("Mead", ItemType.CONSUMABLE, 0, "A delicous glass of mead.")
+    iron_sword = Item("iron sword", ItemType.WEAPON, 5, "A simple iron sword.")
+    fish = Item("small fish", ItemType.MISC, 0, "A small blue fish.")
+    mead = Item("mead", ItemType.CONSUMABLE, 0, "A delicous glass of mead.")
 
     start_zone = Zone("Lake of Thoughts", [POI("Lakefront", "Our adventurer awakens on the lake.", (0, 0), [fish]), 
                                            POI("Shepard's Inn", "A small inn located near the edge of the lake.", (0, 2), [mead])], 
@@ -56,7 +56,6 @@ def main():
 
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Welcome the adventurer to our world.", config=config)
 
-    #print(response.text)
     slow_print_text(response.text, 0.02)
 
     while True:
@@ -65,12 +64,40 @@ def main():
 
         if player_response.strip().lower() in ("quit", "exit"):
             break
-        
-        
+
 
         try:
             response = client.models.generate_content(model="gemini-2.0-flash-001", contents=player_response, config=config)
-            #print(response.text)
+            character_actions = ""
+
+            if (response.function_calls):
+                # Mutate game state based on each function in function calls, maybe one at a time? Update game state, generate new response based on new game state? #
+
+                for function_call in response.function_calls:
+                    print(function_call)
+                    # call function and update game state based on function return
+                    character_actions += f"{call_function(function_call, player)} "
+
+                    print(f"CHARACTER_ACTIONS: {character_actions}")
+                    print(f"CHARACTER STATUS: {player.to_string()}")
+                    # generate new response? Or should called function return a string describing action that occurred?
+                    
+                    # For each function return (string), append to final return string? Feed entire string as player_response to contents of response call?
+
+                    #
+                
+                game_state = f"""
+                    You are an AI Dungeon Master for a text-based adventure game. The adventurer is {player.to_string()}.
+                    Our starting zone is {start_zone.to_string()}.
+
+                    Keep responses between 20 and 120 words.
+
+                    Try to only use information that is provided. Do not invent new zones or locations. Do not list items near player unless they search for them. Feel free to invent smaller details.
+                """
+                config = types.GenerateContentConfig(max_output_tokens=100, system_instruction=game_state, tools=tools)
+                response = client.models.generate_content(model="gemini-2.0-flash-001", contents=character_actions, config=config)
+
+
             slow_print_text(response.text, 0.02)
             
         except Exception as e:
@@ -107,11 +134,23 @@ schema_grab_item = types.FunctionDeclaration(
     ),
 )
 
+# Must wrap function declaration schemes as a types.Tool to pass in as list[Tool] to client config #
 available_functions = types.Tool(
     function_declarations=[
         schema_grab_item
     ]
 )
+
+def call_function(function: types.FunctionCall, character: Character):
+    args = function.args
+    name = function.name
+
+    if (name == "grab_item"):
+        print("calling grab_item")
+        character.grab_item(grab=args["grab"])
+
+        print(f"Char ITEMS: {", ".join(map(Item.to_string, character.items))}")
+        return f"{character.name} grabs {args["grab"]}."
 
 
 if __name__ == "__main__":
