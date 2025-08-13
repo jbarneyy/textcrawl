74f7608e0668388a368577bf3a8cd1451b7a528d
jbarneyy <js_barnett@outlook.com>
2025-08-12 17:07:42 -0700

    Some updates to Enemy() class

--- src/enemy.py
+++ src/enemy.py
@@ -1,6 +1,18 @@
-
+from item import Item
+from poi import POI
 
 class Enemy():
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str, health: int, defence: int, items: list[Item] | None, level: int, current_POI: POI):
+        self.name = name
+        self.health = health
+        self.defence = defence
+        self.items = items
+        self.level = level
+        self.current_POI = current_POI
+
+    
+    def to_string(self):
+        return f"Enemy: {self.name}, Health: {self.health}, Defence: {self.defence}, Level: {self.level}, Current POI: {self.current_POI}"
+    
+    
