50e528fea5d92b212ab2030bef38b29fd4ae7bfd
jbarneyy <js_barnett@outlook.com>
2025-08-12 17:01:17 -0700

    Added Enemy() class for enemies player can fight against, small update to Quest() class

---
+++ src/enemy.py
@@ -0,0 +1,6 @@
+
+
+class Enemy():
+
+    def __init__(self):
+        pass
--- src/quest.py
+++ src/quest.py
@@ -2,5 +2,5 @@
 
 class Quest():
 
-    def __init__(self, name: str, description: str, objectives: list[str], is_complete: bool = False):
+    def __init__(self, name: str, description: str, objectives: list[str], is_complete: bool = False, is_active: bool = False):
         pass
