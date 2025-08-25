bb92f3355ae33b4601d089fe65d96643efa337cc
jbarneyy <js_barnett@outlook.com>
2025-08-24 17:44:28 -0700

    Refactored all function schemas from main.py to function_schemas.py

---
+++ src/function_schemas.py
@@ -0,0 +1,87 @@
+from google.genai import types
+
+schema_grab_item = types.FunctionDeclaration(
+    name="grab_item",
+    description="Grab/pickup an item from the player/character's current POI and place it into character's inventory, if item can_pickup is true. Deletes the item from POI's list of Items.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "grab": types.Schema(
+                type=types.Type.STRING,
+                description="String representing the Item.name of the item player attempts to grab.",
+            ),
+        },
+        required=["grab"]
+    ),
+)
+
+schema_list_items = types.FunctionDeclaration(
+    name="list_items",
+    description="List/display/show character's inventory. Function will return a formatted string displaying the character's inventory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={},
+        required=[]
+    ),
+)
+
+schema_move = types.FunctionDeclaration(
+    name="move",
+    description="Move character from current POI to target_location POI. Takes target POI as argument, returns True if character can move to target_location, else returns False.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "target_location": types.Schema(
+                type=types.Type.OBJECT,
+                description="Target location / POI / Point of Interest that character is attempting to move to."
+            )
+        },
+        required=["target_location"]
+    )
+)
+
+schema_equip_item = types.FunctionDeclaration(
+    name="equip_item",
+    description="Equip an item from the player's inventory into either player's armor var or player's weapon var. Item must be in inventory and of ItemType.WEAPON or ItemType.ARMOR." \
+    "If an item is already equipped, it will move the current equip into player's inventory. Returns True if item is successfully equipped to player.armor or player.weapon. False if equip is unsuccessful.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "item_name": types.Schema(
+                type=types.Type.STRING,
+                description="String representing the name of the item (Item.name) that player is attempting to equip.",
+            ),
+        },
+        required=["item_name"]
+    ),
+)
+
+schema_attack_enemy = types.FunctionDeclaration(
+    name="attack_enemy",
+    description="Attack an enemy if player and enemy are in the same POI. Rolls one attack round of player attacking enemy and enemy attacking player.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "player": types.Schema(
+                type=types.Type.OBJECT,
+                description="Player object representing our player from class Player()"
+            ),
+            "enemy": types.Schema(
+                type=types.Type.OBJECT,
+                description="Enemy object representing the Enemy() our player is attacking."
+            )
+        },
+        required=["player", "enemy"]
+    )
+)
+
+# Must wrap function declaration schemes as a types.Tool to pass in as list[Tool] to client config. #
+available_functions = types.Tool(
+    function_declarations=[
+        schema_grab_item,
+        schema_list_items,
+        schema_move,
+        schema_equip_item,
+        schema_attack_enemy
+    ]
+)
