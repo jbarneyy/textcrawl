dc98e0f33bae304acfb1bbaf207a95858ecf1532
jbarneyy <js_barnett@outlook.com>
2025-09-11 11:16:31 -0700

    Starting work on Quest class, will need to think of way to check quest completion, maybe when update_gamestate() is called

--- src/quest.py
+++ src/quest.py
@@ -3,4 +3,10 @@
 class Quest():
 
     def __init__(self, name: str, description: str, objectives: list[str], is_complete: bool = False, is_active: bool = False):
-        pass
+        self.name = name
+        self.description = description
+        self.objectives = objectives
+        self.is_complete = is_complete
+        self.is_active = is_active
+
+    
